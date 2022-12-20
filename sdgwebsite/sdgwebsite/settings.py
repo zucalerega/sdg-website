1f556fecadf6a45d5f160c32ad79a50be0c199c8
Lizzapid <120133032+Lizzapid@users.noreply.github.com>
2022-12-20 18:11:38 +0000

    updated homepage and css settings

--- sdgwebsite/sdgwebsite/settings.py
+++ sdgwebsite/sdgwebsite/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#Added by Meris
+STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
