a58235464a8f241a486bda34356f6b0ce69a233b
Luca Zerega <laz013laz@gmail.com>
2022-12-21 10:38:23 -0600

    fixed css finally

--- sdgwebsite/sdgwebsite/settings.py
+++ sdgwebsite/sdgwebsite/settings.py
@@ -116,12 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 #Added by Meris
-STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') ##specify static root
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/sdgwebsite/static/',
+]
